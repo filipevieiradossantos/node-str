9dc23e9e8d0d472e61b5f6960b9276de9dca301a
filipevieiradossantos <36301589+filipevieiradossantos@users.noreply.github.com>
2019-07-08 23:45:24 -0300

    Aula 12: Controllers

--- src/routes/index-router.js
+++ src/routes/index-router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Filipe API",
-        version: "0.0.10"
+        version: "0.0.12"
     });
 });
 
--- src/routes/product-router.js
+++ src/routes/product-router.js
@@ -2,21 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/product-controller');
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
+router.post('/', controller.post);
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
 module.exports = router;
